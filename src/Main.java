187b2f3e38c3e7ef0192a241724c9d2b6993a1af
Matt Germeyer <matto483@gmail.com>
2017-03-27 17:01:05 -0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,69 @@
-
 public class Main {
     //COMMAND LINE ARGUMENTS ARE : [0]: 1  [1]: Hi  [2]: True
     public static void main(String[] args) {
+        //prints 'begin'
         System.out.println("begin");
 
+        // FOR ----  for each element arg, in String[] args, do the following:
         for (String arg:args) {
+            //print 2 new lines, followed by 'your arg is' and the arg itself
             System.out.println("\n\nyour arg is: " + arg);
+
+            // take each arg, parse it to an into, store in int variable num.
             int num = Integer.parseInt(arg);
+            //if > 0, tell me..
             if (num > 0) {
                 System.out.println("The int " + num +" is > 0");
+                // if < 0, tell me..
             }else if (num < 0) {
                 System.out.println("The int" + num + " is <= 0");
+                // If == 0, tell me..
             }else {
                 System.out.println("The int" + num + " IS 0!");
             }
 
-
-
+            //Math
             System.out.println("Lets do some math on "+ num );
             int arithmetic = 5 * 2 + (9 % 2 / 3) * 2 - 8;
             System.out.println("Here is some annoying arithmetic " + "5 * 2 + (9 % 2 / 3) * 2 - 8 = 2");
             System.out.println("arithmetic is equal to " + arithmetic);
             int sum = num + arithmetic;
-            System.out.println("The total sum of num + arithmetic " + sum);
+            System.out.println("The total sum of num + arithmetic is " + sum);
 
+
+            //Switch
             switch (arg) {
+                case "0":
+                    System.out.println(arg + "is your arg");
                 case "1":
                     System.out.println(arg + " is the loneliest number!");
                     break;
                 case "2":
-                    System.out.println("2 for the road");
+                    System.out.println(arg + " for the road");
                     break;
                 case "3":
-                    System.out.println("3 to get ready");
+                    System.out.println(arg + " to get ready");
                     break;
                 default:
-                    System.out.println("I guess you didn't give me a number between 1 and 3");
+                    System.out.println(arg + "is a number > 3");
 
 
             }
-
-
-
-
-
-
-
-
-
-            // System.out.println("If your arg has a number, lets multiply it by 2, add 5");
-
-
-            //int num = Integer.parseInt(arg) * 2 +5;
-            //System.out.println(num);
-
-            //int num = Integer.parseInt("2");
-            //System.out.println("num is " + num);
-             /*
-            System.out.println("Lets do some math on num.. ");
-            int arithmetic = (10%3)*(8 / 2) + (1 - 8 + 7);
-            System.out.println(arithmetic);
-            System.out.println("Lets find the sum of num and " +(10%3)*(8 / 2) + (1 - 8 + 7));
-            System.out.println("num + " + arithmetic + " = " + num + arithmetic );
-            */
-
-
-
-
         }
+
+        for (int i = 0; i < args.length; ++i) {
+            System.out.println("your arg is " + args[i]);
+        }
+
+        System.out.println("\n\n after enhanced for loop");
+        int firstarg = Integer.parseInt(args[0]);
+        int secondarg = Integer.parseInt(args[1]);
+        if (firstarg > secondarg){
+            System.out.println(firstarg +" > "+ secondarg);
+        }else {
+            System.out.println(firstarg +" <= "+ secondarg);
+        }
+
 
     }
 }
